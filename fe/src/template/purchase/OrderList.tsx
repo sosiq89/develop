5ed1acf3bc6fe6b9ab81e202791fc41693327e7c
gukhyen <sosiq@kakao.com>
2023-01-26 16:47:18 +0900

    Redux 	- styled component로 div를 생성하는 사용자 정의 태그 생성 	- div 태그 안에 input 사용자 정의 태그를 생성 	- input onClick 이벤트를 적용하여 클릭시 색상 변경 기능 추가

--- fe/src/template/purchase/OrderList.tsx
+++ fe/src/template/purchase/OrderList.tsx
@@ -1,26 +1,35 @@
 import React, { useState } from "react";
-import EditButton from "../common/EditButton";
+import styled from "styled-components";
+
+
+
+const Container = styled.div`
+    border: 5px solid black;
+    padding: 10px;
+`;
+
+const Red = styled(Container)<{backColor:string}>`
+    background-color : ${props => props.backColor};
+`;
+
+const RedInput = (props:any) => {
+    return (
+        <input type="button" value="fire"
+        onClick={(e) => {
+            props.setBackColor("red");
+        }}></input>
+    );
+}
 
 const OrderList = () => {
-    const [count, setCount] = useState(0);
-    const down = () => {
-        setCount(count - 1);
-    }
-    const reset = () => {
-        setCount(0);
-    }
-    const up = () => {
-        setCount(count + 1);
-    }
+    const [backColor, setBackColor] = useState('white');
+
     return (
-        <div>
-            <EditButton EditButton={() => {
-                console.log("EditButton");
-            }}></EditButton> 
-            <input type="button" value="-" onClick={down} />
-            <input type="button" value="0" onClick={reset} />
-            <input type="button" value="+" onClick={up} />
-            <span>{count}</span>
+        <div id="red">
+            <Red backColor={backColor}>
+                <h1>red</h1>
+                <RedInput setBackColor={setBackColor}></RedInput>    
+            </Red>
         </div>
     );
 }
