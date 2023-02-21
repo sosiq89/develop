e39bc1cdcfb1b4145bfb4db5a812275ad913d22a
gukhyen <sosiq@kakao.com>
2023-02-21 11:13:46 +0900

    SpringBoot 	- maven plugin 		— 독립적으로 실행 가능한 JAR SpringBoot 활용 	- SpringApplication 		— banner 변경 	- Application Event 생성 		— Application context 생성 여부와 Bean 객체에 호출여부 관계 확인

---
+++ springstarted/webservershowcase/src/main/java/me/whiteship/webservershowcase/SampleListener.java
@@ -0,0 +1,23 @@
+package me.whiteship.webservershowcase;
+
+import org.springframework.boot.context.event.ApplicationStartingEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+public class SampleListener implements ApplicationListener<ApplicationStartingEvent> {
+  @Override
+  public void onApplicationEvent(ApplicationStartingEvent event) {
+    System.out.println("========================");
+    System.out.println("Application is starting");
+    System.out.println("========================");
+  }
+}
+
+
+
+
+
+
+
+
+
