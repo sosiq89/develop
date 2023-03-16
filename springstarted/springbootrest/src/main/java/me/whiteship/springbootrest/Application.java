c592debe22363c0d855640736a92a5bcbf7d2b27
gukhyen <sosiq@kakao.com>
2023-03-16 15:26:46 +0900

    SpringBoot 	- REST 클라이언트 		— Rest Template 		— Web Client 	- REST 클라이언트 커스터마이징 	- @Bean 이란?

---
+++ springstarted/springbootrest/src/main/java/me/whiteship/springbootrest/Application.java
@@ -0,0 +1,20 @@
+package me.whiteship.springbootrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.reactive.function.client.WebClientCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+  @Bean
+  public WebClientCustomizer webClientCustomizer(){
+    return  webClientBuilder -> webClientBuilder.baseUrl("http://localhost:8080");
+  }
+}
---
+++ springstarted/springbootrest/src/main/java/me/whiteship/springbootrest/RestRunner1.java
@@ -0,0 +1,52 @@
+package me.whiteship.springbootrest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Component
+public class RestRunner1 implements ApplicationRunner {
+  @Autowired
+  WebClient.Builder builder;
+
+  @Override
+  public void run(ApplicationArguments args) throws Exception {
+  /*
+    WebClient webClient = builder.build();
+
+    StopWatch stopWatch = new StopWatch();
+    stopWatch.start();
+
+    Mono<String> helloMono = webClient.get().uri("http://localhost:8080/hello")
+        .retrieve()
+        .bodyToMono(String.class);
+    helloMono.subscribe(s -> {
+      System.out.println(s);
+      if(stopWatch.isRunning()){
+        stopWatch.stop();
+      }
+      System.out.println("1" + stopWatch.prettyPrint());
+      stopWatch.start();
+    });
+
+    Mono<String> worldMono = webClient.get().uri("http://localhost:8080/world")
+        .retrieve()
+        .bodyToMono(String.class);
+    worldMono.subscribe(s -> {
+      System.out.println(s);
+      if(stopWatch.isRunning()){
+        stopWatch.stop();
+      }
+      System.out.println("2" + stopWatch.prettyPrint());
+      stopWatch.start();
+    });
+
+ */
+  }
+}
